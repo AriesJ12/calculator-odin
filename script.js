514ade1bce3adf9152b0b7c66e782db114c3b41a
AriesJ12 <ajtagle12@gmail.com>
2023-09-24 09:30:06 +0800

    revised the operation; still has bugs

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 "use strict";
+let num1OrResult = 0;
+let globalOperator = "";
+let num2OrRaw = 0;
 
+
+//events for numbers
 const btnNumbers = document.querySelectorAll("button[data-type='number']");
 
-btnNumbers.forEach(but => but.addEventListener("click", numberUpdate));
+btnNumbers.forEach((but) => but.addEventListener("click", numberUpdate));
 
+//events for operation
+const btnOperation = document.querySelectorAll("button[data-type='operation']");
 
+btnOperation.forEach((but) => but.addEventListener("click", operationUpdate));
+
+//events for removal
+const btnRemove = document.querySelectorAll("button[data-type='remove']");
+
+btnRemove.forEach(but => but.addEventListener("click", removeUpdate))
 
 //choose the basic function
 function operate(firstNum, operator, secondNum) {
@@ -18,46 +31,62 @@ function operate(firstNum, operator, secondNum) {
     case "/":
       return firstNum / secondNum;
     default:
-      return NaN;
+      return secondNum;
   }
 }
 
-//update the display
-function numberUpdate(event)
-{
+//updates upon clicking an operation
+let freshlyClick = true;
 
+//update the display
+function numberUpdate(event) {
   const VALUE = event.target.dataset.value;
   const DISPLAY = document.querySelector(".display");
 
-
+  if (freshlyClick)
+  {
+    DISPLAY.textContent =  VALUE;
+    freshlyClick = false;
+    return;
+  }
   DISPLAY.textContent = DISPLAY.textContent + VALUE;
 }
 
-function append(event)
-{
-  const TYPE = event.target.dataset.type;
-  const VALUE = event.target.dataset.value;
+//update the operator and do calc if number is more than pair
+function operationUpdate(event) {
+  freshlyClick = true;
   const DISPLAY = document.querySelector(".display");
+  const VALUE = event.target.dataset.value;
 
-  console.log(DISPLAY);
-  if(TYPE === "number")
-  {
-    DISPLAY.textContent = DISPLAY.textContent + VALUE;
-  }
-  else if (TYPE === "operation" && DISPLAY.textContent !== "")
-  {
-    DISPLAY.textContent = `${DISPLAY.textContent} ${VALUE} `;
-  }
-  else
-  {
-    if(VALUE === "clear")
-    {
-      DISPLAY.textContent = "";
-    }
-    else
-    {
-      DISPLAY.textContent = DISPLAY.textContent.slice(0, DISPLAY.textContent.length - 1)
-    }
+  num2OrRaw = +DISPLAY.textContent;
+
+  //stops = from changing the oper
+  if (VALUE !== "=") {
+    globalOperator = VALUE;
   }
 
+  num1OrResult = operate(num1OrResult, globalOperator, num2OrRaw);
+
+  num2OrRaw =0;
+
+  DISPLAY.textContent = num1OrResult;
+}
+
+
+//update upon click of clear and delete(must reset variables when cleared)
+function removeUpdate(event) {
+
+  const DISPLAY = document.querySelector(".display");
+  const VALUE = event.target.dataset.value; 
+  if (VALUE === "clear") {
+    DISPLAY.textContent = "";
+    num1OrResult = 0;
+    globalOperator = "";
+    num2OrRaw = 0;
+  } else {
+    DISPLAY.textContent = DISPLAY.textContent.slice(
+      0,
+      DISPLAY.textContent.length - 1
+    );
+  }
 }
